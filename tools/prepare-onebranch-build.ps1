a6c214f8d783e0e0834433228d16ba1f634927fe
Nick Grifka <nigri@microsoft.com>
2024-01-08 15:01:59 -0800

    Use preparation script adding headers to vpack

---
+++ tools/prepare-onebranch-build.ps1
@@ -0,0 +1,24 @@
+#
+# Preparation script that runs before a OneBranch build.
+#
+# The preparation done in this script involves copying public include files to
+# the vpack directories so they get packaged in the vpack.
+#
+
+Set-StrictMode -Version 'Latest'
+$ErrorActionPreference = 'Stop'
+
+$RootDir = Split-Path $PSScriptRoot -Parent
+$BinDir = Join-Path $RootDir "artifacts\bin"
+$IncludeDir = Join-Path $RootDir "inc"
+
+$Configs = @("chk", "fre")
+$Platforms = @("amd64", "arm64")
+
+foreach ($Config in $Configs) {
+    foreach ($Platform in $Platforms) {
+        $VpackDir = Join-Path $BinDir "$($Platform)$($Config)"
+        New-Item -Path $VpackDir -ItemType Directory -Force | Out-Null
+        Copy-Item $IncludeDir\* $VpackDir -Force | Out-Null
+    }
+}
