7b97e008d8669f3a61a8fc3076e994bbaf6eb810
Michael Friesen (NETWORKING) <3517159+mtfriesen@users.noreply.github.com>
2024-07-11 19:25:22 -0400

    fix length field

--- inc/fnmpapi.h
+++ inc/fnmpapi.h
@@ -413,7 +413,7 @@ FnMpUpdateTaskOffload2(
     In.OffloadParameters = OffloadParameters;
     In.OffloadParametersLength = OffloadParametersLength;
     In.OffloadOptions = OffloadOptions;
-    In.OffloadOptionsLength = sizeof(*OffloadOptions);
+    In.OffloadOptionsLength = OffloadOptions != NULL ? sizeof(*OffloadOptions) : 0;
 
     return
         FnIoctl(Handle, FNMP_IOCTL_MINIPORT_UPDATE_TASK_OFFLOAD, &In, sizeof(In), NULL, 0, NULL, NULL);
